dbfd2bf28fff1632506ccc528467b2f660141a14
Mattia Pellin <mattia@pellin.it>
2020-02-01 11:30:53 +0100

    Release: PlexAPI v0.0.1

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import uvicorn
+
+from fastapi          import FastAPI, Depends, HTTPException
+from routers          import match
+from starlette.status import *
+
+app = FastAPI()
+
+
+async def verify_media_type(media_type: str):
+    if media_type not in {'movie', 'show'}:
+        raise HTTPException(status_code = HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail = "Unsupported Media Type")
+
+
+app.include_router(
+    # import the /match branch of PlexAPI
+    match.router,
+    prefix = '/match',
+    tags   = ['match'],
+    dependencies = [ Depends(verify_media_type) ]
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = "0.0.0.0", port = 8080)
---
+++ routers/match.py
@@ -0,0 +1,100 @@
+from fastapi          import APIRouter, HTTPException
+from plexapi.myplex   import MyPlexAccount
+from starlette.status import *
+
+from tvdb_api_client import TVDBClient
+
+import os
+import logging
+import tmdbsimple as tmdb
+
+router = APIRouter()
+
+
+def env_credentials_check(required_env_vars: list):
+    """Verifies that all required environment variables are set.
+
+    Parameters
+    ----------
+    required_env_vars : list, required
+        The list of environment variables to check
+
+    Raises
+    ------
+    HTTPException
+        If any of the required variables is not set or an empty list is passed in
+    """
+
+    if not all(env_var in os.environ for env_var in required_env_vars) or not required_env_vars:
+        logging.error('Required environment variables not found, raising error...')
+        raise HTTPException(status_code = HTTP_511_NETWORK_AUTHENTICATION_REQUIRED, detail = "Network Authentication Required")
+
+
+@router.get("/", status_code = HTTP_200_OK)
+async def match_all(title: str):
+    raise HTTPException(status_code = HTTP_501_NOT_IMPLEMENTED, detail = "Not Implemented")
+
+
+@router.get("/plex", status_code = HTTP_200_OK)
+async def match_plex(title: str, media_type: str):
+    required_env_vars  = [
+        'PLEXAPI_AUTH_MYPLEX_USERNAME',
+        'PLEXAPI_AUTH_MYPLEX_PASSWORD'
+    ]
+    suggested_env_vars = [
+        'PLEXAPI_PLEXAPI_ENABLE_FAST_CONNECT',
+        'PLEXAPI_PLEXAPI_CONTAINER_SIZE'
+    ]
+    env_credentials_check(required_env_vars)
+    if not all(env_var in os.environ for env_var in suggested_env_vars):
+        logging.warning('Suggested environment variables are not set, proceeding anyway...')
+
+    plex_account  = MyPlexAccount()
+    project_atlas = plex_account.resource('Project: Atlas').connect()
+    plex_results  = project_atlas.search(query = title, mediatype = media_type)
+
+    return [{
+        'guid':  elem.guid,
+        'title': elem.title,
+        'year':  elem.year
+    } for elem in plex_results ]
+
+
+@router.get("/tmdb", status_code = HTTP_200_OK)
+async def match_tmdb(title: str, media_type: str):
+    required_env_vars  = [
+        'TMDB_API_KEY'
+    ]
+    env_credentials_check(required_env_vars)
+
+    tmdb.API_KEY  = os.environ['TMDB_API_KEY']
+    tmdb_search   = tmdb.Search()
+    tmdb_response = tmdb_search.movie(query = title) \
+                    if media_type == 'movie' else \
+                    tmdb_search.tv(query = title)
+    tmdb_results  = tmdb_search.results
+
+    return [{
+        'guid':  elem['id'],
+        'title': elem['title'],
+        'year':  elem['release_date'].split('-')[0] if elem['release_date'] else None
+    } for elem in tmdb_results]
+
+
+@router.get("/tvdb", status_code = HTTP_200_OK)
+async def match_tvdb(title: str, media_type: str):
+    required_env_vars  = [
+        'TVDB_USR_NAME',
+        'TVDB_USR_KEY',
+        'TVDB_API_KEY'
+    ]
+    env_credentials_check(required_env_vars)
+
+    tvdb         = TVDBClient(os.environ['TVDB_USR_NAME'], os.environ['TVDB_USR_KEY'], os.environ['TVDB_API_KEY'])
+    tvdb_results = tvdb.find_series_by_name(title)
+
+    return [{
+        'guid':  elem['tvdb_id'],
+        'title': elem['name'],
+        'year':  elem['air_date'].split('-')[0] if elem['air_date'] else None
+    } for elem in tvdb_results ]
